d333dc154babb82d8ee0c600bed5407a6890be10
tracer-ics2019 <tracer@njuics.org>
2025-12-28 03:42:32 +0800

    >  run U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:42:32 up  7:25,  1 user,  load average: 0.08, 0.10, 0.09 210678e44d0649ec63d3c13392b9b168a0714722

--- nexus-am/am/arch/isa/riscv32.mk
+++ nexus-am/am/arch/isa/riscv32.mk
@@ -1,5 +1,5 @@
 CROSS_COMPILE := riscv-none-embed-
-COMMON_FLAGS  := -fno-pic -march=rv32ima -mabi=ilp32
+COMMON_FLAGS  := -fno-pic -march=rv32ima_zicsr -mabi=ilp32
 CFLAGS        += $(COMMON_FLAGS) -static
 ASFLAGS       += $(COMMON_FLAGS) -O0
 LDFLAGS       += -melf32lriscv
