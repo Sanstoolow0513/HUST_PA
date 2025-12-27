942824365365cc8b407a38eb86612aa575902c4b
tracer-ics2019 <tracer@njuics.org>
2025-12-28 01:26:01 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:26:01 up  5:00,  1 user,  load average: 0.16, 0.44, 0.35 c172630842bcd6281dfc23ac6990eb7aea48f77c

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -2,11 +2,11 @@
 
 make_EHelper(jal){
     //pc+4
-    rtl_addi(&s0, pc, 4);
+    rtl_addi(&s0, &cpu.pc, 4);
     rtl_sr(id_dest->reg, &s0, 4);
 
     //jal
-    rtl_add(&decinfo.jmp_pc, pc, &id_src->val);
+    rtl_add(&decinfo.jmp_pc,&cpu.pc, &id_src->val);
     decinfo.is_jmp = 1;
     
     print_asm_template2(jal);
