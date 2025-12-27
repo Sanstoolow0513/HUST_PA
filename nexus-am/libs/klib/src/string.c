cbdb499fe85fa861b38a4e99766f2f4c169ca3f8
tracer-ics2019 <tracer@njuics.org>
2025-12-28 03:54:50 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:54:50 up  7:38,  1 user,  load average: 0.26, 0.13, 0.09 67a5fb6b81b0134e99f532b0a3970936657ee699

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,82 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
-}
+    const char *p = s;
+    while (*p) p++;
+    return p - s;
+  }
 
-char *strcpy(char* dst,const char* src) {
-  return NULL;
+char *strcpy(char* dst, const char* src) {
+  char *ret = dst;
+  while ((*dst++ = *src++) != '\0');
+  return ret;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char *ret = dst;
+  while (n > 0) {
+    if (*src != '\0') {
+      *dst++ = *src++;
+    } else {
+      *dst++ = '\0';
+    }
+    n--;
+  }
+  return ret;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *ret = dst;
+  while (*dst) dst++;
+  while ((*dst++ = *src++) != '\0');
+  return ret;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  while (n > 0 && *s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+    n--;
+  }
+  if (n == 0) return 0;
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
-void* memset(void* v,int c,size_t n) {
-  return NULL;
+void* memset(void* v, int c, size_t n) {
+  char *p = v;
+  while (n-- > 0) {
+    *p++ = c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *dst = (char*)out;
+  const char *src = (const char*)in;
+  while (n-- > 0) {
+    *dst++ = *src++;
+  }
+  return out;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n){
+int memcmp(const void* s1, const void* s2, size_t n) {
+  const unsigned char *p1 = (const unsigned char*)s1;
+  const unsigned char *p2 = (const unsigned char*)s2;
+  while (n-- > 0) {
+    if (*p1 != *p2) {
+      return *p1 - *p2;
+    }
+    p1++;
+    p2++;
+  }
   return 0;
 }
 
