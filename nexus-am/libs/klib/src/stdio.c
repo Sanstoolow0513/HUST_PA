3b6463ec2f67158c567ff2737153f4c3e391887c
tracer-ics2019 <tracer@njuics.org>
2025-12-28 04:02:57 +0800

    >  run U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:02:57 up  7:47,  1 user,  load average: 0.18, 0.13, 0.09 ffced0a06210b9035b66ee7b1c36790919e9a807

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,20 +3,114 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+static void int_to_str(char *buf, int val) {
+  char tmp[20];
+  int i = 0, j = 0;
+  int is_negative = 0;
+  if (val == 0) {
+    buf[0] = '0';
+    buf[1] = '\0';
+    return;
+  }
+  if (val < 0) {
+    is_negative = 1;
+    val = -val;
+  }
+  while (val > 0) {
+    tmp[i++] = '0' + (val % 10);
+    val /= 10;
+  }
+  if (is_negative) {
+    buf[j++] = '-';
+  }
+  while (i > 0) {
+    buf[j++] = tmp[--i];
+  }
+  buf[j] = '\0';
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  char *p = out;
+
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      switch (*fmt) {
+        case 'd': {
+          int val = va_arg(ap, int);
+          char buf[20];
+          int_to_str(buf, val);
+          char *s = buf;
+          while (*s) {
+            *p++ = *s++;
+          }
+          break;
+        }
+        case 's': {
+          char *s = va_arg(ap, char*);
+          if (s == NULL) {
+            s = "(null)";
+          }
+          while (*s) {
+            *p++ = *s++;
+          }
+          break;
+        }
+        case 'c': {
+          char c = (char)va_arg(ap, int);
+          *p++ = c;
+          break;
+        }
+        case '%': {
+          *p++ = '%';
+          break;
+        }
+        default:
+          *p++ = '%';
+          *p++ = *fmt;
+          break;
+      }
+      fmt++;
+    } else {
+      *p++ = *fmt++;
+    }
+  }
+  *p = '\0';
+  return p - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  int len;
+  va_start(ap, fmt);
+  len = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  int len;
+  va_start(ap, fmt);
+  len = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return len;
+}
+
+int printf(const char *fmt, ...) {
+  char buffer[1024];
+  va_list ap;
+  int len;
+
+  va_start(ap, fmt);
+  len = vsprintf(buffer, fmt, ap);
+  va_end(ap);
+
+  for (int i = 0; i < len; i++) {
+    _putc(buffer[i]);
+  }
+
+  return len;
 }
 
 #endif
