6c9a007ac49517a87a6edbf074bd19e29ceeee26
tracer-ics2019 <tracer@njuics.org>
2025-12-29 12:19:40 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:19:40 up 10 min,  1 user,  load average: 0.00, 0.08, 0.07 eb9bf00d9d8d4b7d3f384f8b4a1f1a8dee7f498e

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -6,8 +6,9 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
+      uint32_t seconds = inl(RTC_ADDR);
       uptime->hi = 0;
-      uptime->lo = 0;
+      uptime->lo = seconds;
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -6,8 +6,9 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      uint32_t screen_info = inl(SCREEN_ADDR);
+      info->width = screen_info >> 16;
+      info->height = screen_info & 0xFFFF;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,7 +19,21 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+      
+      if (ctl->pixels != NULL) {
+        uint32_t *fb = (uint32_t *)FB_ADDR;
+        int x = ctl->x, y = ctl->y;
+        int w = ctl->w, h = ctl->h;
+        uint32_t *pixels = ctl->pixels;
+        int screen_width = inl(SCREEN_ADDR) >> 16;
+        
+        for (int i = 0; i < h; i++) {
+          for (int j = 0; j < w; j++) {
+            fb[(y + i) * screen_width + (x + j)] = pixels[i * w + j];
+          }
+        }
+      }
+      
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
