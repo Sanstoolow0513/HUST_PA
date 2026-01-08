491c739c5c573011cd5cf21e654649c9f0266785
tracer-ics2019 <tracer@njuics.org>
2026-01-09 01:51:08 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:51:08 up  1:47,  1 user,  load average: 0.02, 0.03, 0.00 82afc99b7e4e6e1b8ea31a4ac76a6e6926bb89a4

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -1,56 +1,15 @@
 #include "cpu/exec.h"
 #include "rtl/rtl.h"
 
-make_EHelper(system) {
-  switch (decinfo.isa.instr.funct3) {
-    case 0x0:  // ecall/ebreak等
-      if (decinfo.isa.instr.rd == 0 && 
-          decinfo.isa.instr.rs1 == 0 &&
-          decinfo.isa.instr.funct7 == 0) {
-        // ecall
-        raise_intr(11, cpu.pc);
-        print_asm("ecall");
-      } else {
-        assert(0);
-      }
-      break;
-    case 0x1:  // csrrw
-      exec_csrrw(pc);
-      break;
-    case 0x2:  // csrrs
-      exec_csrrs(pc);
-      break;
-    case 0x3:  // csrrc
-      exec_csrrc(pc);
-      break;
-    case 0x5:  // 根据csr字段区分
-      if (decinfo.isa.instr.csr == 0x102) {  // sret
-        decinfo.jmp_pc = cpu.sepc + 4;
-        decinfo.is_jmp = 1;
-        print_asm("sret");
-      } else {
-        assert(0);
-      }
-      break;
-    default:
-      assert(0);
-  }
-}
-
-
 make_EHelper(ecall) {
-
   raise_intr(11, cpu.pc);
-  
   print_asm("ecall");
 }
 
 make_EHelper(csrrw) {
- 
   uint32_t csr_num = id_src->val;
   uint32_t rs1_val = reg_l(id_src2->reg);
   
-  // 读取CSR的旧值到rd
   uint32_t csr_val = 0;
   switch(csr_num) {
     case 0x100:  // sstatus
@@ -83,7 +42,7 @@ make_EHelper(csrrw) {
       cpu.sepc = rs1_val;
       break;
     case 0x104:  // stvec
-    case 0x105:  // stvec (alternative) 
+    case 0x105:  // stvec (alternative)
       cpu.stvec = rs1_val;
       break;
     case 0x142:  // scause
@@ -97,7 +56,6 @@ make_EHelper(csrrw) {
 }
 
 make_EHelper(csrrs) {
-  // csrrs rd, csr, rs1
   uint32_t csr_num = id_src->val;
   uint32_t rs1_val = reg_l(id_src2->reg);
   
@@ -110,7 +68,7 @@ make_EHelper(csrrs) {
       csr_val = cpu.sepc;
       break;
     case 0x104:  // stvec
-    case 0x105:  // stvec (alternative) 
+    case 0x105:  // stvec (alternative)
       csr_val = cpu.stvec;
       break;
     case 0x142:  // scause
@@ -134,7 +92,7 @@ make_EHelper(csrrs) {
         cpu.sepc = cpu.sepc | rs1_val;
         break;
       case 0x104:  // stvec
-      case 0x105:  // stvec (alternative) 
+      case 0x105:  // stvec (alternative)
         cpu.stvec = cpu.stvec | rs1_val;
         break;
       case 0x142:  // scause
@@ -148,10 +106,91 @@ make_EHelper(csrrs) {
   print_asm("csrrs %s, 0x%x, %s", reg_name(id_dest->reg, 4), csr_num, reg_name(id_src2->reg, 4));
 }
 
-make_EHelper(sret) {
+make_EHelper(csrrc) {
+  uint32_t csr_num = id_src->val;
+  uint32_t rs1_val = reg_l(id_src2->reg);
+  
+  uint32_t csr_val = 0;
+  switch(csr_num) {
+    case 0x100:  // sstatus
+      csr_val = cpu.sstatus;
+      break;
+    case 0x102:  // sepc
+      csr_val = cpu.sepc;
+      break;
+    case 0x104:  // stvec
+    case 0x105:  // stvec
+      csr_val = cpu.stvec;
+      break;
+    case 0x142:  // scause
+      csr_val = cpu.scause;
+      break;
+    default:
+      printf("Unknown CSR: 0x%x\n", csr_num);
+      assert(0);
+  }
+  
+  if (id_dest->reg != 0) {
+    rtl_sr(id_dest->reg, &csr_val, 4);
+  }
+  
+  if (id_src2->reg != 0) {
+    switch(csr_num) {
+      case 0x100:  // sstatus
+        cpu.sstatus = cpu.sstatus & ~rs1_val;
+        break;
+      case 0x102:  // sepc
+        cpu.sepc = cpu.sepc & ~rs1_val;
+        break;
+      case 0x104:  // stvec
+      case 0x105:  // stvec
+        cpu.stvec = cpu.stvec & ~rs1_val;
+        break;
+      case 0x142:  // scause
+        cpu.scause = cpu.scause & ~rs1_val;
+        break;
+      default:
+        assert(0);
+    }
+  }
+  
+  print_asm("csrrc %s, 0x%x, %s", reg_name(id_dest->reg, 4), csr_num, reg_name(id_src2->reg, 4));
+}
 
+make_EHelper(sret) {
   decinfo.jmp_pc = cpu.sepc + 4;
   decinfo.is_jmp = 1;
-  
   print_asm("sret");
+}
+
+make_EHelper(system) {
+  switch (decinfo.isa.instr.funct3) {
+    case 0x0:  // ecall/ebreak等
+      if (decinfo.isa.instr.rd == 0 && 
+          decinfo.isa.instr.rs1 == 0 &&
+          decinfo.isa.instr.funct7 == 0) {
+        exec_ecall(pc);
+      } else {
+        assert(0);
+      }
+      break;
+    case 0x1:  // csrrw
+      exec_csrrw(pc);
+      break;
+    case 0x2:  // csrrs
+      exec_csrrs(pc);
+      break;
+    case 0x3:  // csrrc
+      exec_csrrc(pc);
+      break;
+    case 0x5:  // 根据csr字段区分
+      if (decinfo.isa.instr.csr == 0x102) {  // sret
+        exec_sret(pc);
+      } else {
+        assert(0);
+      }
+      break;
+    default:
+      assert(0);
+  }
 }
