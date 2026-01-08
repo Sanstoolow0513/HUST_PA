ad9f4f8ad642524f5c62032acb58587ead38f97a
tracer-ics2019 <tracer@njuics.org>
2026-01-09 01:52:10 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:52:10 up  1:48,  1 user,  load average: 0.09, 0.05, 0.01 5d3040bccf7e708d9c92300d29f0a6c198c67b17

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -1,167 +1,7 @@
 #include "cpu/exec.h"
 #include "rtl/rtl.h"
 
-make_EHelper(ecall) {
-  raise_intr(11, cpu.pc);
-  print_asm("ecall");
-}
-
-make_EHelper(csrrw) {
-  uint32_t csr_num = id_src->val;
-  uint32_t rs1_val = reg_l(id_src2->reg);
-  
-  uint32_t csr_val = 0;
-  switch(csr_num) {
-    case 0x100:  // sstatus
-      csr_val = cpu.sstatus;
-      break;
-    case 0x102:  // sepc
-      csr_val = cpu.sepc;
-      break;
-    case 0x104:  // stvec
-    case 0x105:  // stvec (alternative encoding)
-      csr_val = cpu.stvec;
-      break;
-    case 0x142:  // scause
-      csr_val = cpu.scause;
-      break;
-    default:
-      printf("Unknown CSR: 0x%x\n", csr_num);
-      assert(0);
-  }
-
-  if (id_dest->reg != 0) {
-    rtl_sr(id_dest->reg, &csr_val, 4);
-  }
-  
-  switch(csr_num) {
-    case 0x100:  // sstatus
-      cpu.sstatus = rs1_val;
-      break;
-    case 0x102:  // sepc
-      cpu.sepc = rs1_val;
-      break;
-    case 0x104:  // stvec
-    case 0x105:  // stvec (alternative)
-      cpu.stvec = rs1_val;
-      break;
-    case 0x142:  // scause
-      cpu.scause = rs1_val;
-      break;
-    default:
-      assert(0);
-  }
-  
-  print_asm("csrrw %s, 0x%x, %s", reg_name(id_dest->reg, 4), csr_num, reg_name(id_src2->reg, 4));
-}
-
-make_EHelper(csrrs) {
-  uint32_t csr_num = id_src->val;
-  uint32_t rs1_val = reg_l(id_src2->reg);
-  
-  uint32_t csr_val = 0;
-  switch(csr_num) {
-    case 0x100:  // sstatus
-      csr_val = cpu.sstatus;
-      break;
-    case 0x102:  // sepc
-      csr_val = cpu.sepc;
-      break;
-    case 0x104:  // stvec
-    case 0x105:  // stvec (alternative)
-      csr_val = cpu.stvec;
-      break;
-    case 0x142:  // scause
-      csr_val = cpu.scause;
-      break;
-    default:
-      printf("Unknown CSR: 0x%x\n", csr_num);
-      assert(0);
-  }
-  
-  if (id_dest->reg != 0) {
-    rtl_sr(id_dest->reg, &csr_val, 4);
-  }
-  
-  if (id_src2->reg != 0) {
-    switch(csr_num) {
-      case 0x100:  // sstatus
-        cpu.sstatus = cpu.sstatus | rs1_val;
-        break;
-      case 0x102:  // sepc
-        cpu.sepc = cpu.sepc | rs1_val;
-        break;
-      case 0x104:  // stvec
-      case 0x105:  // stvec (alternative)
-        cpu.stvec = cpu.stvec | rs1_val;
-        break;
-      case 0x142:  // scause
-        cpu.scause = cpu.scause | rs1_val;
-        break;
-      default:
-        assert(0);
-    }
-  }
-  
-  print_asm("csrrs %s, 0x%x, %s", reg_name(id_dest->reg, 4), csr_num, reg_name(id_src2->reg, 4));
-}
-
-make_EHelper(csrrc) {
-  uint32_t csr_num = id_src->val;
-  uint32_t rs1_val = reg_l(id_src2->reg);
-  
-  uint32_t csr_val = 0;
-  switch(csr_num) {
-    case 0x100:  // sstatus
-      csr_val = cpu.sstatus;
-      break;
-    case 0x102:  // sepc
-      csr_val = cpu.sepc;
-      break;
-    case 0x104:  // stvec
-    case 0x105:  // stvec
-      csr_val = cpu.stvec;
-      break;
-    case 0x142:  // scause
-      csr_val = cpu.scause;
-      break;
-    default:
-      printf("Unknown CSR: 0x%x\n", csr_num);
-      assert(0);
-  }
-  
-  if (id_dest->reg != 0) {
-    rtl_sr(id_dest->reg, &csr_val, 4);
-  }
-  
-  if (id_src2->reg != 0) {
-    switch(csr_num) {
-      case 0x100:  // sstatus
-        cpu.sstatus = cpu.sstatus & ~rs1_val;
-        break;
-      case 0x102:  // sepc
-        cpu.sepc = cpu.sepc & ~rs1_val;
-        break;
-      case 0x104:  // stvec
-      case 0x105:  // stvec
-        cpu.stvec = cpu.stvec & ~rs1_val;
-        break;
-      case 0x142:  // scause
-        cpu.scause = cpu.scause & ~rs1_val;
-        break;
-      default:
-        assert(0);
-    }
-  }
-  
-  print_asm("csrrc %s, 0x%x, %s", reg_name(id_dest->reg, 4), csr_num, reg_name(id_src2->reg, 4));
-}
-
-make_EHelper(sret) {
-  decinfo.jmp_pc = cpu.sepc + 4;
-  decinfo.is_jmp = 1;
-  print_asm("sret");
-}
+void raise_intr(uint32_t NO, vaddr_t epc);
 
 make_EHelper(system) {
   switch (decinfo.isa.instr.funct3) {
@@ -169,28 +9,131 @@ make_EHelper(system) {
       if (decinfo.isa.instr.rd == 0 && 
           decinfo.isa.instr.rs1 == 0 &&
           decinfo.isa.instr.funct7 == 0) {
-        exec_ecall(pc);
+        // ecall
+        raise_intr(11, cpu.pc);
+        print_asm("ecall");
       } else {
         assert(0);
       }
       break;
-    case 0x1:  // csrrw
-      exec_csrrw(pc);
+      
+    case 0x1: {  // csrrw
+      uint32_t csr_num = decinfo.isa.instr.csr;
+      uint32_t rs1_val = reg_l(decinfo.isa.instr.rs1);
+      uint32_t rd = decinfo.isa.instr.rd;
+      
+      uint32_t csr_val = 0;
+      switch(csr_num) {
+        case 0x100: csr_val = cpu.sstatus; break;
+        case 0x102: csr_val = cpu.sepc; break;
+        case 0x104: 
+        case 0x105: csr_val = cpu.stvec; break;
+        case 0x142: csr_val = cpu.scause; break;
+        default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
+      }
+      
+      if (rd != 0) {
+        rtl_sr(rd, &csr_val, 4);
+      }
+      
+      switch(csr_num) {
+        case 0x100: cpu.sstatus = rs1_val; break;
+        case 0x102: cpu.sepc = rs1_val; break;
+        case 0x104: 
+        case 0x105: cpu.stvec = rs1_val; break;
+        case 0x142: cpu.scause = rs1_val; break;
+        default: assert(0);
+      }
+      
+      print_asm("csrrw %s, 0x%x, %s", reg_name(rd, 4), csr_num, reg_name(decinfo.isa.instr.rs1, 4));
       break;
-    case 0x2:  // csrrs
-      exec_csrrs(pc);
+    }
+    
+    case 0x2: {  // csrrs
+      uint32_t csr_num = decinfo.isa.instr.csr;
+      uint32_t rs1_val = reg_l(decinfo.isa.instr.rs1);
+      uint32_t rd = decinfo.isa.instr.rd;
+      
+      uint32_t csr_val = 0;
+      switch(csr_num) {
+        case 0x100: csr_val = cpu.sstatus; break;
+        case 0x102: csr_val = cpu.sepc; break;
+        case 0x104: 
+        case 0x105: csr_val = cpu.stvec; break;
+        case 0x142: csr_val = cpu.scause; break;
+        default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
+      }
+      
+      if (rd != 0) {
+        rtl_sr(rd, &csr_val, 4);
+      }
+      
+      if (decinfo.isa.instr.rs1 != 0) {
+        switch(csr_num) {
+          case 0x100: cpu.sstatus |= rs1_val; break;
+          case 0x102: cpu.sepc |= rs1_val; break;
+          case 0x104: 
+          case 0x105: cpu.stvec |= rs1_val; break;
+          case 0x142: cpu.scause |= rs1_val; break;
+          default: assert(0);
+        }
+      }
+      
+      print_asm("csrrs %s, 0x%x, %s", reg_name(rd, 4), csr_num, reg_name(decinfo.isa.instr.rs1, 4));
       break;
-    case 0x3:  // csrrc
-      exec_csrrc(pc);
+    }
+    
+    case 0x3: {  // csrrc
+      uint32_t csr_num = decinfo.isa.instr.csr;
+      uint32_t rs1_val = reg_l(decinfo.isa.instr.rs1);
+      uint32_t rd = decinfo.isa.instr.rd;
+      
+      uint32_t csr_val = 0;
+      switch(csr_num) {
+        case 0x100: csr_val = cpu.sstatus; break;
+        case 0x102: csr_val = cpu.sepc; break;
+        case 0x104: 
+        case 0x105: csr_val = cpu.stvec; break;
+        case 0x142: csr_val = cpu.scause; break;
+        default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
+      }
+      
+      if (rd != 0) {
+        rtl_sr(rd, &csr_val, 4);
+      }
+      
+      if (decinfo.isa.instr.rs1 != 0) {
+        switch(csr_num) {
+          case 0x100: cpu.sstatus &= ~rs1_val; break;
+          case 0x102: cpu.sepc &= ~rs1_val; break;
+          case 0x104: 
+          case 0x105: cpu.stvec &= ~rs1_val; break;
+          case 0x142: cpu.scause &= ~rs1_val; break;
+          default: assert(0);
+        }
+      }
+      
+      print_asm("csrrc %s, 0x%x, %s", reg_name(rd, 4), csr_num, reg_name(decinfo.isa.instr.rs1, 4));
       break;
+    }
+    
     case 0x5:  // 根据csr字段区分
       if (decinfo.isa.instr.csr == 0x102) {  // sret
-        exec_sret(pc);
+        decinfo.jmp_pc = cpu.sepc + 4;
+        decinfo.is_jmp = 1;
+        print_asm("sret");
       } else {
         assert(0);
       }
       break;
+      
     default:
       assert(0);
   }
+}
+
+// 保留ecall单独的helper（如果需要）
+make_EHelper(ecall) {
+  raise_intr(11, cpu.pc);
+  print_asm("ecall");
 }
