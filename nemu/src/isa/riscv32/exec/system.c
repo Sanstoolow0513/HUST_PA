eeffadb5aad3b82538c422144291d635852e8390
tracer-ics2019 <tracer@njuics.org>
2026-01-15 16:37:12 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:37:12 up 33 min,  1 user,  load average: 0.18, 0.09, 0.09 19e897706a92f70021ad0c37d6b829b178bd3ae

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -36,7 +36,7 @@ make_EHelper(system) {
         case 0x102: csr_val = cpu.sepc; break;
         case 0x104: 
         case 0x105: csr_val = cpu.stvec; break;
-        case 0x141: csr_val = cpu.stvec; break;  
+        case 0x141: csr_val = cpu.sepc; break;  
         case 0x142: csr_val = cpu.scause; break;
         default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
       }
@@ -50,7 +50,7 @@ make_EHelper(system) {
         case 0x102: cpu.sepc = rs1_val; break;
         case 0x104: 
         case 0x105: cpu.stvec = rs1_val; break;
-        case 0x141: cpu.stvec = rs1_val; break;  
+        case 0x141: cpu.sepc = rs1_val; break;  
         case 0x142: cpu.scause = rs1_val; break;
         default: assert(0);
       }
@@ -70,7 +70,7 @@ make_EHelper(system) {
         case 0x102: csr_val = cpu.sepc; break;
         case 0x104: 
         case 0x105: csr_val = cpu.stvec; break;
-        case 0x141: csr_val = cpu.stvec; break; 
+        case 0x141: csr_val = cpu.sepc; break; 
         case 0x142: csr_val = cpu.scause; break;
         default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
       }
@@ -85,7 +85,7 @@ make_EHelper(system) {
           case 0x102: cpu.sepc |= rs1_val; break;
           case 0x104: 
           case 0x105: cpu.stvec |= rs1_val; break;
-          case 0x141: cpu.stvec |= rs1_val; break;  
+          case 0x141: cpu.sepc |= rs1_val; break;  
           case 0x142: cpu.scause |= rs1_val; break;
           default: assert(0);
         }
@@ -106,7 +106,7 @@ make_EHelper(system) {
         case 0x102: csr_val = cpu.sepc; break;
         case 0x104: 
         case 0x105: csr_val = cpu.stvec; break;
-        case 0x141: csr_val = cpu.stvec; break; 
+        case 0x141: csr_val = cpu.sepc; break; 
         case 0x142: csr_val = cpu.scause; break;
         default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
       }
@@ -121,7 +121,7 @@ make_EHelper(system) {
           case 0x102: cpu.sepc &= ~rs1_val; break;
           case 0x104: 
           case 0x105: cpu.stvec &= ~rs1_val; break;
-          case 0x141: cpu.stvec &= ~rs1_val; break;
+          case 0x141: cpu.sepc &= ~rs1_val; break;
           case 0x142: cpu.scause &= ~rs1_val; break;
           default: assert(0);
         }
