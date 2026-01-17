297016c1f402d004ba33b6513eae9f3ca788728f
tracer-ics2019 <tracer@njuics.org>
2026-01-18 05:12:38 +0800

    >  compile U202215561 sanstoolow Linux DESKTOP-CNBRSK5 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  05:12:38 up  3:40,  1 user,  load average: 0.22, 0.16, 0.28 d889ab3c23d867bb97546b16f09cd7a67cf74921

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -137,6 +137,82 @@ make_EHelper(system) {
       break;
     }
     
+    case 0x6: {  // csrrsi (CSR read and set bits immediate)
+      uint32_t csr_num = decinfo.isa.instr.csr;
+      uint32_t zimm = decinfo.isa.instr.rs1;  // 5-bit immediate in rs1 field
+      uint32_t rd = decinfo.isa.instr.rd;
+      
+      uint32_t csr_val = 0;
+      switch(csr_num) {
+        case 0x100: csr_val = cpu.sstatus; break;
+        case 0x102: csr_val = cpu.sepc; break;
+        case 0x104: 
+        case 0x105: csr_val = cpu.stvec; break;
+        case 0x141: csr_val = cpu.sepc; break;  
+        case 0x142: csr_val = cpu.scause; break;
+        case 0x180: csr_val = cpu.satp; break;
+        default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
+      }
+      
+      if (rd != 0) {
+        rtl_sr(rd, &csr_val, 4);
+      }
+      
+      if (zimm != 0) {  // Only update if immediate is non-zero
+        switch(csr_num) {
+          case 0x100: cpu.sstatus |= zimm; break;
+          case 0x102: cpu.sepc |= zimm; break;
+          case 0x104: 
+          case 0x105: cpu.stvec |= zimm; break;
+          case 0x141: cpu.sepc |= zimm; break;  
+          case 0x142: cpu.scause |= zimm; break;
+          case 0x180: cpu.satp |= zimm; break;
+          default: assert(0);
+        }
+      }
+      
+      print_asm("csrrsi %s, 0x%x, %d", reg_name(rd, 4), csr_num, zimm);
+      break;
+    }
+    
+    case 0x7: {  // csrrci (CSR read and clear bits immediate)
+      uint32_t csr_num = decinfo.isa.instr.csr;
+      uint32_t zimm = decinfo.isa.instr.rs1;  // 5-bit immediate in rs1 field
+      uint32_t rd = decinfo.isa.instr.rd;
+      
+      uint32_t csr_val = 0;
+      switch(csr_num) {
+        case 0x100: csr_val = cpu.sstatus; break;
+        case 0x102: csr_val = cpu.sepc; break;
+        case 0x104: 
+        case 0x105: csr_val = cpu.stvec; break;
+        case 0x141: csr_val = cpu.sepc; break; 
+        case 0x142: csr_val = cpu.scause; break;
+        case 0x180: csr_val = cpu.satp; break;
+        default: printf("Unknown CSR: 0x%x\n", csr_num); assert(0);
+      }
+      
+      if (rd != 0) {
+        rtl_sr(rd, &csr_val, 4);
+      }
+      
+      if (zimm != 0) {  // Only update if immediate is non-zero
+        switch(csr_num) {
+          case 0x100: cpu.sstatus &= ~zimm; break;
+          case 0x102: cpu.sepc &= ~zimm; break;
+          case 0x104: 
+          case 0x105: cpu.stvec &= ~zimm; break;
+          case 0x141: cpu.sepc &= ~zimm; break;
+          case 0x142: cpu.scause &= ~zimm; break;
+          case 0x180: cpu.satp &= ~zimm; break;
+          default: assert(0);
+        }
+      }
+      
+      print_asm("csrrci %s, 0x%x, %d", reg_name(rd, 4), csr_num, zimm);
+      break;
+    }
+    
     case 0x5:  
       assert(0); // funct3=5不应该出现
       break;
